123c2770f1a8d3f70c2c1c1a0abf4b4afd4f3831
Narek <narek1985ch@gmail.com>
2022-02-02 20:00:41 +0100

    useCollback

--- src/app/components/examples/useCallback/useCallBackExample.jsx
+++ src/app/components/examples/useCallback/useCallBackExample.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 
 const UseCallBackExample = () => {
+    const [data, setData] = useState({});
+    const withoutCallback = useRef(0);
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({ ...prevState, [target.name]: target.value }));
+    };
+    const validateWithoutCallback = (data) => {
+        console.log(data);
+    };
+    useEffect(() => {
+        validateWithoutCallback(data);
+    }, [data]);
+    useEffect(() => {
+        withoutCallback.current++;
+    }, [validateWithoutCallback]);
+
     return (
         <CardWrapper>
             <SmallTitle>Example</SmallTitle>
+            <p>Render withoutCallback: {withoutCallback.current}</p>
+            <label htmlFor="email" className="form-label">
+                Email
+            </label>
+            <input
+                onChange={handleChange}
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                value={data.email || ""}
+            />
         </CardWrapper>
     );
 };
