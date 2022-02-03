16168452f5a18475e0ae05c8c3defcee5208b47c
Narek <narek1985ch@gmail.com>
2022-02-03 08:52:12 +0100

    react.memo

--- src/app/components/examples/memo/memoWithUseCallbackExample.jsx
+++ src/app/components/examples/memo/memoWithUseCallbackExample.jsx
@@ -1,3 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const LogOutButton = ({ onLogOut }) => {
+    return <button className="btn btn-primary" onClick={onLogOut}></button>;
+};
+
+LogOutButton.propTypes = {
+    onLogOut: PropTypes.func
+};
+
 const MemoWithUseCallbackExample = (props) => {
     return null;
 };
