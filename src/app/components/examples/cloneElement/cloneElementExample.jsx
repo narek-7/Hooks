5ab4264b647789407a33edce773411b0c08e6879
Narek <narek1985ch@gmail.com>
2022-02-06 19:08:31 +0100

    Children

--- src/app/components/examples/cloneElement/cloneElementExample.jsx
+++ src/app/components/examples/cloneElement/cloneElementExample.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import CardWrapper from "../../common/Card";
+import TextField from "../../common/form/textField";
 
 import SmallTitle from "../../common/typografy/smallTitle";
 const CloneElementExample = () => {
+    const field = <TextField name="email" label="email" />;
+
+    const handleChange = (target) => {
+        console.log("change", target);
+    };
     return (
         <CardWrapper>
             <SmallTitle>Пример</SmallTitle>
+            {/* Its only for reading, thats why we need to craete the clon of field!  */}
+            {field}
+            {React.cloneElement(field, {
+                onChange: handleChange,
+                label: "Cloned email"
+            })}
         </CardWrapper>
     );
 };
